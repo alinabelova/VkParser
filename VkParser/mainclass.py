eb0003a3642568c13c4875e569195a08c10f41d2
Alina <alina_belova94@mail.ru>
2019-04-12 21:25:31 +0700

    3 группы

--- VkParser/mainclass.py
+++ VkParser/mainclass.py
@@ -8,7 +8,7 @@ from time import sleep
 import time
 import csv
 
-class post(object):
+class Post(object):
     def __init__(self, id, text, datetime, ownerId, likesCount, repostsCount, commentsCount, viewsCount):
         self.id = id
         self.text = text
@@ -18,6 +18,11 @@ class post(object):
         self.repostsCount = repostsCount
         self.commentsCount = commentsCount
         self.viewsCount = viewsCount
+class PostEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Post):
+            return obj.__dict__
+        return json.JSONEncoder.default(self, obj)
 
 class mywindow(QtWidgets.QMainWindow): 
     def __init__(self):
@@ -34,11 +39,15 @@ class mywindow(QtWidgets.QMainWindow):
         search_phrases = self.ui.textEdit.toPlainText()
         group_id = self.ui.lineEdit.text()
         date_start = self.ui.dateTimeEdit.dateTime().toMSecsSinceEpoch()
-        parse_group(group_id, date_start)
+        parse_group('34183390', date_start) #https://vk.com/public34183390
+        parse_group('54767216', date_start) #https://vk.com/kraschp
+        parse_group('59804801', date_start) #https://vk.com/krsk_overhear
 
-def write_json(data):
-    with open('posts.json', 'w', encoding="utf-8") as file:
-        json.dump(data, file, indent=2, ensure_ascii=False)
+def write_json(data, id):
+    with open('posts' + id + '.json', 'w', encoding="utf-8") as outfile:
+        json.dump(data, outfile, cls=PostEncoder, ensure_ascii=False)
+  #  with open('posts.json', 'w', encoding="utf-8") as file:
+   #     json.dump(data, file, indent=2, ensure_ascii=False)
 def write_csv_headers():
     with open('posts_data.csv', 'a', newline='') as file:
         writer = csv.writer(file)
@@ -78,6 +87,10 @@ def get_data(post):
         reposts = post['reposts']['count']
     except:
         reposts = 0
+    try:
+        views = post['views']['count']
+    except:
+        views = 0
   #  data = {
    #     id: post_id,
    #     datetime: time.strftime("%d.%m.%Y, %H:%M:%S", time.localtime(post['date'])),
@@ -86,12 +99,13 @@ def get_data(post):
      #   comments: comments,
       #  reposts: reposts
        # }
-    data = post(post_id, post['text'], time.strftime("%d.%m.%Y, %H:%M:%S", time.localtime(post['date'])), 1, post_likes, reposts, comments, 0)
+  
+    data = Post(post_id, post['text'], time.strftime("%d.%m.%Y, %H:%M:%S", time.localtime(post['date'])), post['owner_id'], post_likes, reposts, comments, views)
     return data
 
 def parse_group(group, date_start):
-  #  group_id = '-' + group
-    group_id = '-34183390'
+    group_id = '-' + group
+   # group_id = '-34183390'
     offset = 0
     all_posts = []
 
@@ -106,13 +120,12 @@ def parse_group(group, date_start):
    #         break
     data_posts = []
    #  write_csv_headers()
-    for post in all_posts:
-        data_posts.extend(get_data(post))
+    for p in all_posts:
+        data_posts.append(get_data(p))
     #    post_data = get_data(post)
   #       write_csv(post_data)
 
-    print(len(all_posts))
-    write_json(data_posts.json())
+    write_json(data_posts, group_id)
 
 #if __name__ == '__main__':
  #   main()
